d6faecfa0254c92c37802e4295efa6e0155946f1
Sharique <shariquefaizi@gmail.com>
2026-03-16 18:55:03 +0530

    fffff

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,9 @@ import fs from "fs";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
-import { mochaPlugins } from "@getmocha/vite-plugins";
 
 export default defineConfig({
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   plugins: [
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ...mochaPlugins(process.env as any),
     react(),
     (() => {
       const emailsConfigPath =
